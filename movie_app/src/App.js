f1d68a0ade93c2d19467a0e73ed19538ddbfadb0
Junseok <smartse0k@naver.com>
2020-04-16 03:23:55 +0900

    this.props를 사용해 자식 컴포넌트에 데이터 넘기기 구현

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './App.css';
 import Movie from './Movie';
 
+const movieTitles = [
+    "너의 이름은",
+    "마도카 마기카 반역의 이야기",
+    "목소리의 형태",
+    "날씨의 아이",
+];
+
+const movieImages = [
+    "https://upload.wikimedia.org/wikipedia/ko/thumb/d/d1/%EB%84%88%EC%9D%98_%EC%9D%B4%EB%A6%84%EC%9D%80.jpg/260px-%EB%84%88%EC%9D%98_%EC%9D%B4%EB%A6%84%EC%9D%80.jpg",
+    "https://t1.daumcdn.net/cfile/tistory/277D21495326D9032D",
+    "https://lh3.googleusercontent.com/proxy/ubdXbbbTouBgdKoD9LxjRWu6W1A26pUWSKifStZofnrvy0YIC0Y3X2N_ax7FBnDknIFybBeMgoX7GyK2GkLzyPPpXseT3EAyzmyM1g-iB_c_EVQIO8hAahGtDfatO_hL3eWeEtjCb2wQCY3M90rq-OsN3b4AeH-Zi9p9kFKyaKPJn6o0pP-xyQ8ADwlkYSoP6BFPlTyfrae45W2DHQzJ8AgXC2ainXVn6dNIohXa7J-Y3sz4V3zA0VpbGYn3U-qlvMTEUlHO3RJX9g3pB8sfsBYoilbVgyIK148GNj-ALVfGc4lz5jPpJA",
+    "https://file2.nocutnews.co.kr/newsroom/image/2019/11/04/20191104174023351654_0_800_1144.jpg",
+];
+
 function App() {
     return (
         <div className="App">
-            <Movie />
-            <Movie />
-            <Movie />
-            <Movie />
+            <Movie title={movieTitles[0]} poster={movieImages[0]} />
+            <Movie title={movieTitles[1]} poster={movieImages[1]} />
+            <Movie title={movieTitles[2]} poster={movieImages[2]} />
+            <Movie title={movieTitles[3]} poster={movieImages[3]} />
         </div>
     );
 }
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -5,8 +5,8 @@ class Movie extends Component {
     render() {
         return (
             <div>
-                <h1>1232132</h1>
-                <MoviePoster />
+                <h1>{this.props.title}</h1>
+                <MoviePoster poster={this.props.poster} />
             </div>
         );
     }
@@ -15,7 +15,7 @@ class Movie extends Component {
 class MoviePoster extends Component {
     render() {
         return (
-            <img src="https://upload.wikimedia.org/wikipedia/ko/thumb/f/f5/%EB%82%A0%EC%94%A8%EC%9D%98_%EC%95%84%EC%9D%B4.tif/lossless-page1-1200px-%EB%82%A0%EC%94%A8%EC%9D%98_%EC%95%84%EC%9D%B4.tif.png" />
+            <img src={this.props.poster} />
         );
     }
 }
