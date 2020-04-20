6facc34adc594763a4e5480994f44fe4db4ad1e2
Junseok <smartse0k@naver.com>
2020-04-20 22:13:04 +0900

    functional 컴포넌트에서 매개변수? 넘기기 실습

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import PropTypes from 'prop-types';
 import './Movie.css';
 
+/*
 class Movie extends Component {
     static propTypes = {
         title: PropTypes.string.isRequired,
@@ -17,7 +18,23 @@ class Movie extends Component {
         );
     }
 }
+*/
 
+function Movie({title, poster}) {
+    return (
+        <div>
+            <h1>{title}</h1>
+            <MoviePoster poster={poster} />
+        </div>
+    );
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+}
+
+/*
 class MoviePoster extends Component {
     static propTypes = {
         poster: PropTypes.string.isRequired
@@ -29,5 +46,16 @@ class MoviePoster extends Component {
         );
     }
 }
+*/
+
+function MoviePoster({poster}) {
+    return (
+        <img src={poster} alt="" />
+    );
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
+};
 
 export default Movie
