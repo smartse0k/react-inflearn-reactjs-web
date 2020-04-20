718cbc1ddef50e26d8f1ecc25e008c0952d81859
Junseok <smartse0k@naver.com>
2020-04-20 23:07:27 +0900

    async와 await를 사용하여 ajax 호출하고 결과 받기 실습

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -73,16 +73,27 @@ class App extends Component {
     */
 
     componentDidMount() {
-        fetch("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
-            .then(response => response.json())
-            .then(json => console.log(json))
-            .catch(err => console.log(err));
+        this._getMovies();
     }
+
+    _getMovies = async () => {
+        const movies = await this._callApi();
+        this.setState({
+            movies
+        });
+    };
+
+    _callApi = () => {
+        return fetch("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
+            .then(response => response.json())
+            .then(json => json.data.movies)
+            .catch(err => console.log(err));
+    };
 
     _renderMovies = () => {
         const movies = this.state.movies.map((movie, index)=> {
             return (
-                <Movie key={index} title={movie.title} poster={movie.poster} />
+                <Movie key={movie.id} title={movie.title} poster={movie.large_cover_image} />
             )
         });
 
